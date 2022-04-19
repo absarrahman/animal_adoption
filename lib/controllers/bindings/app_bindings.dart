8f036e4819a4b1e0e43595ca4a64fa7aed2669f6
absarrahman <raman040635@gmail.com>
2022-04-19 13:25:39 +0600

    Fix Post controller
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lib/controllers/bindings/app_bindings.dart
+++ lib/controllers/bindings/app_bindings.dart
@@ -2,10 +2,11 @@ import 'package:animal_adoption/controllers/post_controller.dart';
 import 'package:get/get.dart';
 
 import '../auth_controller.dart';
+
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(AuthController());
-    Get.put(PostController());
+    Get.lazyPut(() => PostController(), fenix: true);
   }
 }
