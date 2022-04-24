b3afaebfee6c01a2475586157724cf4ac35de175
absarrahman <raman040635@gmail.com>
2022-04-24 12:15:06 +0600

    Initial layout and logic for view post history
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lib/views/view_post.dart
+++ lib/views/view_post.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../constants/string_constants.dart';
+import '../services/firebase_api.dart';
 
 class ViewPostHistory extends StatelessWidget {
   const ViewPostHistory({Key? key}) : super(key: key);
@@ -6,10 +12,46 @@ class ViewPostHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("data"),
+        child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: FirebaseAPI.getCollectionRef(collectionPath: FireStoreConstants.adoptionPosts)
+                .where('userUuid', isEqualTo: FirebaseAPI.firebaseAuth.currentUser!.uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                log(FirebaseAPI.firebaseAuth.currentUser!.uid);
+                List adoptionPostList = snapshot.data!.docs.toList();
+                log("${adoptionPostList.length}");
+                return ListView.builder(
+                    itemCount: adoptionPostList.length,
+                    itemBuilder: (context, index) {
+                      var adoptionPost = adoptionPostList[index].data();
+                      return UserPostObserveWidget(adoptionPost: adoptionPost);
+                    });
+              } else if (snapshot.hasError) {
+                return const Text("Something went wrong");
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator.adaptive());
+              } else {
+                return const Text("Please try again");
+              }
+            }),
       ),
     );
+  }
+}
+
+class UserPostObserveWidget extends StatelessWidget {
+  const UserPostObserveWidget({
+    Key? key,
+    required this.adoptionPost,
+  }) : super(key: key);
+
+  final Map<String, dynamic> adoptionPost;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(adoptionPost.toString());
   }
 }
