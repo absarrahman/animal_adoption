2af2c795e5ce09ea3dfe357b1fa1db6a48605b51
absarrahman <raman040635@gmail.com>
2022-04-25 10:23:18 +0600

    Initial user rating design
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- lib/views/view_post.dart
+++ lib/views/view_post.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 
 import '../constants/string_constants.dart';
@@ -58,19 +59,99 @@ class UserPostObserveWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //Image
-          CircleAvatar(
-            radius: 70,
-            foregroundImage: NetworkImage(adoptionPost[ModelConstants.imageUrl]),
-          ),
-          //Post name
-          Text(adoptionPost[ModelConstants.postName]),
-          // Booked UUID
-          Text(adoptionPost[ModelConstants.bookedUuid])
-        ],
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            //Image
+            CircleAvatar(
+              radius: 70,
+              foregroundImage: NetworkImage(adoptionPost[ModelConstants.imageUrl]),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            //Post name
+            Text(adoptionPost[ModelConstants.postName]),
+            const SizedBox(
+              width: 20,
+            ),
+            // Booked UUID
+            // If book user exists, option to reject/accept, else blank/ not booked yet
+            adoptionPost[ModelConstants.bookedUuid] != null
+                ? ElevatedButton(
+                    onPressed: (() {
+                      Get.defaultDialog(
+                        title: "User Info",
+                        cancel: ElevatedButton(
+                            onPressed: () {
+                              // Set booked user to null
+                            },
+                            child: const Text("Reject")),
+                        confirm: ElevatedButton(
+                            onPressed: () {
+                              Get.defaultDialog(
+                                title: "Rate the user",
+                                  content: Column(
+                                children: [
+                                  const Text("Share your experience with the user"),
+                                  RatingBar.builder(
+                                    initialRating: 3,
+                                    minRating: 1,
+                                    direction: Axis.horizontal,
+                                    allowHalfRating: true,
+                                    itemCount: 5,
+                                    itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                    itemBuilder: (context, _) => const Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                    ),
+                                    onRatingUpdate: (rating) {
+                                      print(rating);
+                                    },
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      // Set isBooked to true, set the value in db
+                                    },
+                                    child: const Text("Rate"),
+                                  )
+                                ],
+                              ));
+                            },
+                            child: const Text("Accept")),
+                        content: FutureBuilder(
+                            future: FirebaseAPI.getCollectionRef(collectionPath: FireStoreConstants.userCollection)
+                                .doc(adoptionPost[ModelConstants.userUuid])
+                                .get(),
+                            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                              if (snapshot.hasData) {
+                                var data = snapshot.data!;
+                                return Column(
+                                  children: [
+                                    // Email
+                                    Text(data[ModelConstants.email]),
+                                    // Rating
+                                    // if avg rate and totalRateCount = 0, user did not adopt any animals before
+                                    const Text("User average rating"),
+                                  ],
+                                );
+                              } else {
+                                return const Text("Failed to retrieve data");
+                              }
+                            }),
+                      );
+                    }),
+                    child: const Text("Someone booked it. Tap to check"),
+                  )
+                : Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text("Not booked yet"),
+                  ),
+          ],
+        ),
       ),
     );
   }
